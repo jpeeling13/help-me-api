b1d9dcc82453cd49d49fedbbb46f8c7840d9508b
Joshua Peeling <jpeeling13@outlook.com>
2020-05-21 23:43:48 -0400

    added basic request api w/fake data store

--- server/main.go
+++ server/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
 	"encoding/json"
+	"github.com/jpeeling13/help-me-api/platform/requestFeed"
 )
 
 func main() {
+	tds := requestFeed.New()
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Welcome to the Help API Server"))
+		w.Write([]byte("Welcome to the Help Me API Server"))
 	})
 
 	r.Get("/requests", func(w http.ResponseWriter, r *http.Request){
-
-		json.NewEncoder(w)
-
+		requests := tds.GetAll()
+		json.NewEncoder(w).Encode(requests)
 	})
 
+	r.Post("/requests", func(w http.ResponseWriter, r *http.Request){
+		newRequest := map[string]string{}
+		json.NewDecoder(r.Body).Decode(&newRequest)
+		tds.Add(requestFeed.Request{
+			newRequest["Description"],
+			newRequest["RequesterName"],
+		})
+		w.Write([]byte("Thanks for the new request!"))
+	})
+
+	fmt.Println("Starting help-me-api server")
 	http.ListenAndServe(":3000", r)
 }
